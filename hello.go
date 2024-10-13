ce7729aeeb14b3f75e336245c5755824d1e394cb
VinitGurjar <vinitfagna@gmail.com>
2024-10-13 17:21:40 +0530

    feat(init setup): Implement makefile with fmt, vet and build

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(2 + 3)
+
+	fmt.Println("Hello aizen")
+
+	fmt.Println(2 * 3)
+
+	foo()
+
+}
+
+func foo() {
+
+	var guess bool
+	fmt.Println(guess)
+}
